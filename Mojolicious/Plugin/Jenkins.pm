8bfc012d8067e08c4e050e92c1d62d5b2a67080a
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-03-05 16:09:38 +1300

    wrote basic jenkins plugin

--- Mojolicious/Plugin/Jenkins.pm
+++ Mojolicious/Plugin/Jenkins.pm
@@ -1,15 +1,29 @@
 package Mojolicious::Plugin::Jenkins;
 use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::UserAgent;
 
 sub register {
-	my ($plugin, $app, $conf) = @_;
-	
-	# default values
-	
-	$app->helper(
-		process_commit => sub {
-            my ($project, $doc) = @_;
-            return;
+    my ( $plugin, $app, $conf ) = @_;
+
+    # default values
+
+    $app->helper(
+        process_commit => sub {
+            my ( $couch, $doc ) = @_;
+            my $config = $couch->get_view(
+                {
+                    view => 'repository/config',
+                    opts => {
+                        include_docs => 'true',
+                        key          => $doc->{repository}->{name}
+                    }
+                }
+            );
+            return unless $config->{ $doc->{repository}->{name} };
+            my $url = $config->{ $doc->{repository}->{name} }->{jenkins};
+            return unless $url;
+            my $ua = Mojo::UserAgent->new;
+            return $ua->get($url);
         },
     );
 }
