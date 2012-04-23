ed986aa100f17afea17967bf69b328ecd65421eb
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-04-23 14:56:46 +1200

    fixed the jabber plugin - now supports groupchat

--- Mojolicious/Plugin/Jabber.pm
+++ Mojolicious/Plugin/Jabber.pm
@@ -14,15 +14,27 @@ sub register {
 
     my $j  = AE::cv;
     my $cl = AnyEvent::XMPP::Client->new();
+ 
+    my ($disco, $version, $muc, $acc);
 
     $app->helper(
         start_jabber => sub {
             my ($self) = @_;
+            if ($params->{chatroom}) {
+                $disco   = AnyEvent::XMPP::Ext::Disco->new;
+                $version = AnyEvent::XMPP::Ext::Version->new;
+                $muc     = AnyEvent::XMPP::Ext::MUC->new( disco => $disco );
+            }
             $cl->add_account( $params->{user}, $params->{pass} );
             $cl->reg_cb(
                 session_ready => sub {
-                    my ( $cl, $acc ) = @_;
+                    my ( $cl, $_acc ) = @_;
+                    $acc = $_acc;
                     $app->log->debug("Connected to jabber server");
+                    if ($params->{chatroom}) {
+                        $muc->join_room( $acc->connection, $params->{chatroom}, 'githublicious' );
+                        $self->set_jabber_presence('boring ...');
+                    }
                 },
                 disconnect => sub {
                     my ( $cl, $acc, $h, $p, $reas ) = @_;
@@ -39,8 +51,25 @@ sub register {
     $app->helper(
         send_jabber => sub {
             my ( $self, $msg, $to ) = @_;
-            my $cnf = 1 if ( $to =~ m{conference} );
-            $cl->send_message( $msg => $to, undef, 'chat' );
+            if($params->{chatroom}) {
+                $to = $params->{chatroom};
+                my $muc_msg = AnyEvent::XMPP::Ext::MUC::Message->new(
+                    type => 'groupchat',
+                    to   => $to,
+                    body => $msg
+                );
+                $muc_msg->{connection} = $acc->connection,
+                $muc_msg->{room} = $muc->get_room( $acc->connection, $to );
+                $muc_msg->send();
+            } else {
+                $cl->send_message( $msg => $to, undef, 'chat' );
+            }
+        },
+    );
+    $app->helper(
+        set_jabber_presence => sub {
+            my ( $self, $presence ) = @_;
+            $cl->set_presence( undef, $presence, 1 );
         },
     );
 }
